1b3844b6911bbe3d2fe1f86a7b0c1a1678f62faf
Marko Goronjic <goronjicmarko24@gmail.com>
2021-09-15 09:39:31 +0200

    Drawing on the Canvas

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,9 @@
+const canvas = document.getElementById('gameScreen');
 
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = '#0ff';
+ctx.fillRect(10, 10, 100, 100);
+
+ctx.fillStyle = '#00f';
+ctx.fillRect(200, 200, 30, 30);
