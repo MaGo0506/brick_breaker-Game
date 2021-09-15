811e9c52828e3fe5de24373292979dfcccb673a8
Marko Goronjic <goronjicmarko24@gmail.com>
2021-09-16 01:16:22 +0200

    Collision Detection between the Balll and the Paddle

--- src/app/utils/ball.js
+++ src/app/utils/ball.js
@@ -6,6 +6,8 @@ export default class Ball {
     this.position = { x: 10, y: 10 };
     this.speed = { x: 4, y: 2 };
 
+    this.game = game;
+
     this.gameWidth = game.gameWidth;
     this.gameHeight = game.gameHeight;
   }
@@ -24,11 +26,28 @@ export default class Ball {
     this.position.x += this.speed.x;
     this.position.y += this.speed.y;
 
+    // wall left or right
     if (this.position.x > this.gameWidth - this.size || this.position.x < 0) {
       this.speed.x = -this.speed.x;
     }
+
+    // wall top or bottom
     if (this.position.y > this.gameHeight - this.size || this.position.y < 0) {
       this.speed.y = -this.speed.y;
+    }
+
+    // check collision with paddle
+    const bottomOfBall = this.position.y + this.size;
+    const topOfPaddle = this.game.paddle.position.y;
+    const leftSideOfPaddle = this.game.paddle.position.x;
+    const rightSideOfPaddle = this.game.paddle.position.x + this.game.paddle.width;
+
+    if (bottomOfBall >= topOfPaddle
+        && this.position.x >= leftSideOfPaddle
+        && this.position.x + this.size <= rightSideOfPaddle
+    ) {
+      this.speed.y = -this.speed.y;
+      this.position.y = this.game.paddle.position.y - this.size;
     }
   }
 }
